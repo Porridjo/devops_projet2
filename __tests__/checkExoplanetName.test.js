df84a153575a095e8ef21a33b36cec131d144650
Porridjo <den.nguyen69@gmail.com>
2022-12-23 03:33:03 +0100

    remove tests

--- __tests__/checkExoplanetName.test.js
+++
@@ -1,7 +0,0 @@
-const { checkName } = require('../utils/utils')
-
-describe('checkCapitalName', () => {
-    it('should return true if all letters are capital', () => {
-        expect(checkName('TEST')).toBe(true)
-    })
-})
--- utils/utils.js
+++
@@ -1,5 +0,0 @@
-module.exports.checkName = (name) => {
-    const regex = /^[A-Za-z\.-]*$/
-    const result = regex.test(name)
-    return result
-}
